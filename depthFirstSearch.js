14ae86d87ca37d0afcb6806e25d104e7b16a54a4
jhoun <jayhoun@gmail.com>
2017-01-27 20:30:02 -1000

    passed 15th test

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -4,6 +4,29 @@ const Node = require('./graphGenerator');
 
 const DFS = (start, searchFor) => {
 
+
+
+  let stack = [];
+  let visited = [];
+  let currentNode = start;
+
+  stack.push(start);
+
+
+  while (stack.length !==0){
+    currentNode = stack.pop();
+    if(currentNode.value === searchFor){
+      return currentNode;
+    }
+    if (visited.indexOf(currentNode) !== 1) {
+      visited.push(currentNode.name);
+      currentNode.neighbors.forEach(function(element){
+        stack.push(element);
+      })
+    }
+  }
+    return false;
+
 }
 
 module.exports = DFS;
